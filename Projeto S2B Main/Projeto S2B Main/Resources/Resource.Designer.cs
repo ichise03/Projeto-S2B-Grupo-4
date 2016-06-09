a573cc5c3d34b74f1b058c8addf36a4f0716e2bb
Marcel <marcel.silva@sga.pucminas.br>
2016-06-09 14:58:08 -0300

    Scroll na tela de lançamentos

--- Projeto S2B Main/Projeto S2B Main/Resources/Resource.Designer.cs
+++ Projeto S2B Main/Projeto S2B Main/Resources/Resource.Designer.cs
@@ -60,110 +60,125 @@ namespace Projeto_S2B_Main
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f05000d
-			public const int autoCompleteTextView1 = 2131034125;
-			
-			// aapt resource value: 0x7f05000e
-			public const int button1 = 2131034126;
-			
-			// aapt resource value: 0x7f050002
-			public const int categorias = 2131034114;
-			
-			// aapt resource value: 0x7f050006
-			public const int categoriasView = 2131034118;
-			
-			// aapt resource value: 0x7f050017
-			public const int contaButton = 2131034135;
-			
-			// aapt resource value: 0x7f050001
-			public const int contas = 2131034113;
-			
-			// aapt resource value: 0x7f050008
-			public const int contasView = 2131034120;
-			
-			// aapt resource value: 0x7f050007
-			public const int criarCategoria = 2131034119;
-			
-			// aapt resource value: 0x7f050009
-			public const int criarConta = 2131034121;
-			
-			// aapt resource value: 0x7f050020
-			public const int criarLancamento = 2131034144;
-			
-			// aapt resource value: 0x7f050022
-			public const int criarTransferencia = 2131034146;
-			
-			// aapt resource value: 0x7f05000b
-			public const int editText1 = 2131034123;
+			// aapt resource value: 0x7f05001e
+			public const int ScrollViewID = 2131034142;
 			
 			// aapt resource value: 0x7f050015
-			public const int editText2 = 2131034133;
+			public const int autoCompleteTextView1 = 2131034133;
 			
-			// aapt resource value: 0x7f050016
-			public const int excluirConta = 2131034134;
+			// aapt resource value: 0x7f05000b
+			public const int button1 = 2131034123;
 			
-			// aapt resource value: 0x7f050003
-			public const int lancamentos = 2131034115;
-			
-			// aapt resource value: 0x7f05001f
-			public const int lancamentosView = 2131034143;
-			
-			// aapt resource value: 0x7f050010
-			public const int radioButton1 = 2131034128;
+			// aapt resource value: 0x7f05000d
+			public const int categorias = 2131034125;
 			
 			// aapt resource value: 0x7f050011
-			public const int radioButton2 = 2131034129;
+			public const int categoriasView = 2131034129;
 			
-			// aapt resource value: 0x7f050012
-			public const int radioButton3 = 2131034130;
+			// aapt resource value: 0x7f05001d
+			public const int contaButton = 2131034141;
+			
+			// aapt resource value: 0x7f05000c
+			public const int contas = 2131034124;
 			
 			// aapt resource value: 0x7f050013
-			public const int radioButton4 = 2131034131;
+			public const int contasView = 2131034131;
 			
-			// aapt resource value: 0x7f05000f
-			public const int radioGroup1 = 2131034127;
-			
-			// aapt resource value: 0x7f050005
-			public const int sair = 2131034117;
+			// aapt resource value: 0x7f050012
+			public const int criarCategoria = 2131034130;
 			
 			// aapt resource value: 0x7f050014
-			public const int saldoLabel = 2131034132;
+			public const int criarConta = 2131034132;
+			
+			// aapt resource value: 0x7f050025
+			public const int criarLancamento = 2131034149;
+			
+			// aapt resource value: 0x7f050027
+			public const int criarTransferencia = 2131034151;
+			
+			// aapt resource value: 0x7f050006
+			public const int editText1 = 2131034118;
+			
+			// aapt resource value: 0x7f050008
+			public const int editText2 = 2131034120;
+			
+			// aapt resource value: 0x7f05000a
+			public const int editText3 = 2131034122;
+			
+			// aapt resource value: 0x7f050023
+			public const int editText4 = 2131034147;
+			
+			// aapt resource value: 0x7f05001c
+			public const int excluirConta = 2131034140;
+			
+			// aapt resource value: 0x7f05000e
+			public const int lancamentos = 2131034126;
+			
+			// aapt resource value: 0x7f050024
+			public const int lancamentosView = 2131034148;
+			
+			// aapt resource value: 0x7f050017
+			public const int radioButton1 = 2131034135;
 			
 			// aapt resource value: 0x7f050018
-			public const int spinner1 = 2131034136;
+			public const int radioButton2 = 2131034136;
 			
 			// aapt resource value: 0x7f050019
-			public const int spinner2 = 2131034137;
+			public const int radioButton3 = 2131034137;
+			
+			// aapt resource value: 0x7f05001a
+			public const int radioButton4 = 2131034138;
+			
+			// aapt resource value: 0x7f050016
+			public const int radioGroup1 = 2131034134;
+			
+			// aapt resource value: 0x7f050010
+			public const int sair = 2131034128;
 			
 			// aapt resource value: 0x7f05001b
-			public const int spinner3 = 2131034139;
+			public const int saldoLabel = 2131034139;
+			
+			// aapt resource value: 0x7f050002
+			public const int spinner1 = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int spinner2 = 2131034116;
+			
+			// aapt resource value: 0x7f05001f
+			public const int spinner3 = 2131034143;
 			
 			// aapt resource value: 0x7f050000
 			public const int textLabel = 2131034112;
 			
-			// aapt resource value: 0x7f05000a
-			public const int textView1 = 2131034122;
+			// aapt resource value: 0x7f050001
+			public const int textView1 = 2131034113;
 			
-			// aapt resource value: 0x7f05000c
-			public const int textView2 = 2131034124;
+			// aapt resource value: 0x7f050003
+			public const int textView2 = 2131034115;
 			
-			// aapt resource value: 0x7f05001a
-			public const int textView3 = 2131034138;
+			// aapt resource value: 0x7f050005
+			public const int textView3 = 2131034117;
 			
-			// aapt resource value: 0x7f05001c
-			public const int textView4 = 2131034140;
+			// aapt resource value: 0x7f050007
+			public const int textView4 = 2131034119;
 			
-			// aapt resource value: 0x7f05001d
-			public const int textView5 = 2131034141;
+			// aapt resource value: 0x7f050009
+			public const int textView5 = 2131034121;
 			
-			// aapt resource value: 0x7f05001e
-			public const int textView6 = 2131034142;
+			// aapt resource value: 0x7f050020
+			public const int textView6 = 2131034144;
 			
 			// aapt resource value: 0x7f050021
-			public const int tranferenciasView = 2131034145;
+			public const int textView7 = 2131034145;
 			
-			// aapt resource value: 0x7f050004
-			public const int transferencia = 2131034116;
+			// aapt resource value: 0x7f050022
+			public const int textView8 = 2131034146;
+			
+			// aapt resource value: 0x7f050026
+			public const int tranferenciasView = 2131034150;
+			
+			// aapt resource value: 0x7f05000f
+			public const int transferencia = 2131034127;
 			
 			static Id()
 			{
@@ -182,31 +197,34 @@ namespace Projeto_S2B_Main
 			public const int celula = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int demonstrativotransferencia = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int telacategorias = 2130903042;
+			public const int Main = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int telacontas = 2130903043;
+			public const int telacategorias = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int telacriarcategoria = 2130903044;
+			public const int telacontas = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int telacriarconta = 2130903045;
+			public const int telacriarcategoria = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int telafazertransferencia = 2130903046;
+			public const int telacriarconta = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int telagerarlancamento = 2130903047;
+			public const int telafazertransferencia = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int telalancamentos = 2130903048;
+			public const int telagerarlancamento = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int telatransferencias = 2130903049;
+			public const int telalancamentos = 2130903049;
+			
+			// aapt resource value: 0x7f03000a
+			public const int telatransferencias = 2130903050;
 			
 			static Layout()
 			{
