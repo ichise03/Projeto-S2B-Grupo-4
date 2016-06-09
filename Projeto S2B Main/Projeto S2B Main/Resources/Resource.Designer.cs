5b3b0b4db436b5a8d03eaea6fea0f0ef57b73c36
Lucasrsv1 <lucasrsv1@gmail.com>
2016-06-09 14:58:19 -0300

    Ok

--- Projeto S2B Main/Projeto S2B Main/Resources/Resource.Designer.cs
+++ Projeto S2B Main/Projeto S2B Main/Resources/Resource.Designer.cs
@@ -182,31 +182,34 @@ namespace Projeto_S2B_Main
 			public const int celula = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int Main = 2130903041;
+			public const int detalhesDeLancamentos = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int telacategorias = 2130903042;
+			public const int Main = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int telacontas = 2130903043;
+			public const int telacategorias = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int telacriarcategoria = 2130903044;
+			public const int telacontas = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int telacriarconta = 2130903045;
+			public const int telacriarcategoria = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int telafazertransferencia = 2130903046;
+			public const int telacriarconta = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int telagerarlancamento = 2130903047;
+			public const int telafazertransferencia = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int telalancamentos = 2130903048;
+			public const int telagerarlancamento = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int telatransferencias = 2130903049;
+			public const int telalancamentos = 2130903049;
+			
+			// aapt resource value: 0x7f03000a
+			public const int telatransferencias = 2130903050;
 			
 			static Layout()
 			{
